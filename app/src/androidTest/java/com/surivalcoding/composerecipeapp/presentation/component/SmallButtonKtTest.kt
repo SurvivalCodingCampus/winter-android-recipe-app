05f866638a64af34195724a7e24a6bdd06e7148b
choe-yujin <dev.yujinchoe@gmail.com>
2025-01-14 21:01:52 +0900

    test(SmallButton): add unit tests for click behavior

--- app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/SmallButtonKtTest.kt
+++ app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/SmallButtonKtTest.kt
@@ -1,12 +1,32 @@
 package com.surivalcoding.composerecipeapp.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class SmallButtonKtTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
 
     @Test
     fun smallButton() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            SmallButton(
+                buttonText = "Small button",
+                onClick = {
+                    isClicked = true
+                }
+            )
+        }
+
+        composeTestRule.onNodeWithText("Small button").performClick()
+
+        assertTrue(isClicked)
     }
 }
