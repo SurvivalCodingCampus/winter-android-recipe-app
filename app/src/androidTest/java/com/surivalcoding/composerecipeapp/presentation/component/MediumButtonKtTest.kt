31ae45acf515c3f1675a8beb61948ce6d45aee96
choe-yujin <dev.yujinchoe@gmail.com>
2025-01-14 21:46:53 +0900

    test(MediumButton): add tests for diabled state

--- app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/MediumButtonKtTest.kt
+++ app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/MediumButtonKtTest.kt
@@ -1,5 +1,7 @@
 package com.surivalcoding.composerecipeapp.presentation.component
 
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -13,20 +15,47 @@ class MediumButtonKtTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun mediumButton() {
+    fun mediumButton_should_handle_click_when_enabled() {
         var isClicked = false
 
         composeTestRule.setContent {
             MediumButton(
                 buttonText = "Medium button",
+                enabled = true,
                 onClick = {
                     isClicked = true
                 }
             )
         }
 
-        composeTestRule.onNodeWithText("Medium button").performClick()
+        composeTestRule.onNodeWithText("Medium button").apply {
+            assertIsEnabled()
+            performClick()
+        }
 
         assertTrue(isClicked)
     }
+
+    @Test
+    fun mediumButton_should_not_handle_click_when_disabled() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            MediumButton(
+                buttonText = "Medium button",
+                enabled = false,
+                onClick = {
+                    isClicked = true
+                }
+            )
+        }
+
+        composeTestRule.onNodeWithText("Medium button").apply {
+            assertIsNotEnabled()
+            performClick()
+        }
+
+        assertFalse(isClicked)
+    }
+
 }
