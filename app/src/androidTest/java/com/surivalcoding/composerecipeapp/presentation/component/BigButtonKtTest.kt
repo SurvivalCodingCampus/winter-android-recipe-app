ec5db2cc82b08eceedbee1813ac78a2fbdbe0373
최현정 <hjchoi.dev@gmail.com>
2025-01-14 21:15:25 +0900

    refactor: Reorganize package structure and file locations

--- app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/BigButtonKtTest.kt
+++ app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/BigButtonKtTest.kt
@@ -3,6 +3,7 @@ package com.surivalcoding.composerecipeapp.presentation.component
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import com.surivalcoding.composerecipeapp.ui.component.BigButton
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
