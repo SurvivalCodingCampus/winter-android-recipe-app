28766b2bc0e3363f6c07e427598df8b576413282
hodak <jlkim909@gmail.com>
2025-01-15 02:04:58 +0900

    feat: theme 및 font 설정

---
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/CraIcons.kt
@@ -0,0 +1,9 @@
+package com.surivalcoding.composerecipeapp.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.ArrowForward
+
+object CraIcons {
+    val ArrowForward = Icons.AutoMirrored.Rounded.ArrowForward
+}
--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Type.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Type.kt
@@ -2,33 +2,58 @@ package com.surivalcoding.composerecipeapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.surivalcoding.composerecipeapp.R
 
 // Set of Material typography styles to start with
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+)
+
 val Typography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+    bodySmall = TextStyle(
+        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
+        lineHeight = 16.5.sp,
+        letterSpacing = 0.4.sp,
+    ),
+)
+
+
+val CustomTypography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
     )
-    */
 )
