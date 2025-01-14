39d45dee44ce1336124c06de93068b21f8bd75b7
choe-yujin <dev.yujinchoe@gmail.com>
2025-01-15 02:21:58 +0900

    feat(InputField): create text input component

---
+++ app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/InputFieldTest.kt
@@ -0,0 +1,12 @@
+package com.surivalcoding.composerecipeapp.presentation.component
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class InputFieldTest {
+
+    @Test
+    fun inputField() {
+    }
+}
---
+++ app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/TabsTest.kt
@@ -0,0 +1,12 @@
+package com.surivalcoding.composerecipeapp.presentation.component
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class TabsTest {
+
+    @Test
+    fun tabs() {
+    }
+}
