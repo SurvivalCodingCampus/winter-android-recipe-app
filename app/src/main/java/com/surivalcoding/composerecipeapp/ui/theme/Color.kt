0f5318c1bf110d84dd78c50635663bb59cfed927
최현정 <hjchoi.dev@gmail.com>
2025-01-14 21:23:54 +0900

    style: Add Natural Color and Primary Color

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Color.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.surivalcoding.composerecipeapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Black = Color(0xFF000000)
+val Gray1 = Color(0xFF484848)
+val Gray2 = Color(0xFF797979)
+val Gray3 = Color(0xFFA9A9A9)
+val Gray4 = Color(0xFFD9D9D9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary100 = Color(0xFF129575)
+val Primary80 = Color(0xFF71B1A1)
+val Primary60 = Color(0xFFAFD3CA)
+val Primary40 = Color(0xFFDBEBE7)
+val Primary20 = Color(0xFFF6FAF9)
