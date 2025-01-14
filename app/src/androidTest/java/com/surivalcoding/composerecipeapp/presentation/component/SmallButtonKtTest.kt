75c863447d3f9470edc9856fceecfaf30afd97c1
choe-yujin <dev.yujinchoe@gmail.com>
2025-01-14 21:47:18 +0900

    test(SmallButton): add tests for diabled state

--- app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/SmallButtonKtTest.kt
+++ app/src/androidTest/java/com/surivalcoding/composerecipeapp/presentation/component/SmallButtonKtTest.kt
@@ -1,5 +1,6 @@
 package com.surivalcoding.composerecipeapp.presentation.component
 
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -13,7 +14,7 @@ class SmallButtonKtTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun smallButton() {
+    fun smallButton_should_handle_click_when_enabled() {
         var isClicked = false
 
         composeTestRule.setContent {
@@ -29,4 +30,27 @@ class SmallButtonKtTest {
 
         assertTrue(isClicked)
     }
+
+    @Test
+    fun smallButton_should_not_handle_click_when_disabled() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            SmallButton(
+                buttonText = "Small button",
+                enabled = false,
+                onClick = {
+                    isClicked = true
+                }
+            )
+        }
+
+        composeTestRule.onNodeWithText("Small button").apply {
+            assertIsNotEnabled()
+            performClick()
+        }
+
+        assertFalse(isClicked)
+    }
+
 }
