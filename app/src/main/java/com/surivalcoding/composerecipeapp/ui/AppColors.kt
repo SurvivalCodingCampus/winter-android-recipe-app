ef49df5c2ca7db6d2638fce25d46e65f3272d109
lcw <skcjddn@gmail.com>
2025-01-15 11:09:07 +0900

    feat : add AppTextStyles and AppColors
    - Add `normalTextRegular` style in `AppTextStyles`.
    - Add `placeHolder`, `inputFieldBoardDefault`, `inputFieldBoardFocus`, `white` and `primary80` colors in `AppColors`.
    - add W400 font weight in AppTextStyle

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/AppColors.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/AppColors.kt
@@ -4,4 +4,9 @@ import androidx.compose.ui.graphics.Color
 
 object AppColors {
     val primary = Color(0xFF129575)
+    val placeHolder = Color(0xFFD9D9D9)
+    val inputFieldBoardDefault = Color(0xFFD9D9D9)
+    val inputFieldBoardFocus = Color(0xFF71B1A1)
+    val white = Color(0xFFFFFFFF)
+    val primary80 = Color(0xFF71B1A1)
 }
