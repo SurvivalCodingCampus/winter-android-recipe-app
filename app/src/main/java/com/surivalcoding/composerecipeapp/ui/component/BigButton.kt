e2439c98c685d04524c7ac83a3f67f8c0e934a7d
최현정 <hjchoi.dev@gmail.com>
2025-01-15 23:20:33 +0900

    feat: Update BigButton

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/component/BigButton.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/component/BigButton.kt
@@ -17,23 +17,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.surivalcoding.composerecipeapp.ui.AppColors
-import com.surivalcoding.composerecipeapp.ui.AppTextStyles
+import com.surivalcoding.composerecipeapp.ui.theme.PoppinsBoldTypography
+import com.surivalcoding.composerecipeapp.ui.theme.Primary100
 
 @Composable
 fun BigButton(
     modifier: Modifier = Modifier,
     text: String,
+    iconSizePercent: Float = 1.0f,
     onClick: () -> Unit = {},
 ) {
+    val configuration = LocalConfiguration.current
+    val fontScale = configuration.fontScale
+    val baseIconSize = 20.dp
+    val calculateIconSize = baseIconSize * iconSizePercent * fontScale
+
     Box(
         modifier = modifier
             .width(315.dp)
             .height(60.dp)
             .background(
-                color = AppColors.primary,
+                color = Primary100,
                 shape = RoundedCornerShape(10.dp),
             )
             .clickable {
@@ -51,14 +58,14 @@ fun BigButton(
             ) {
                 Text(
                     text = text,
-                    style = AppTextStyles.normalTextBold.copy(
+                    style = PoppinsBoldTypography.bodyLarge.copy(
                         color = Color.White,
                     ),
                 )
             }
             Spacer(modifier = Modifier.width(11.dp))
             Icon(
-                modifier = Modifier.size(20.dp),
+                modifier = Modifier.size(calculateIconSize),
                 imageVector = Icons.AutoMirrored.Default.ArrowForward,
                 contentDescription = null,
                 tint = Color.White,
@@ -69,7 +76,7 @@ fun BigButton(
 
 @Preview(showBackground = true)
 @Composable
-private fun BigButtonPreview() {
+private fun MediumButtonPreview() {
     BigButton(
         text = "Button"
     )
