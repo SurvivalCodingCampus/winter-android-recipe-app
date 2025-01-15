62704d040f807a6bccab3a07da985ccbadc5408d
최현정 <hjchoi.dev@gmail.com>
2025-01-15 13:36:23 +0900

    style: Add missing colors

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Color.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Color.kt
@@ -10,6 +10,10 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+// Font color
+val DefaultFontColor = Color(0xFF121212)
+
+// Natural colors
 val Black = Color(0xFF000000)
 val Gray1 = Color(0xFF484848)
 val Gray2 = Color(0xFF797979)
@@ -21,3 +25,20 @@ val Primary80 = Color(0xFF71B1A1)
 val Primary60 = Color(0xFFAFD3CA)
 val Primary40 = Color(0xFFDBEBE7)
 val Primary20 = Color(0xFFF6FAF9)
+
+// Secondary colors
+val Secondary100 = Color(0xFFFF9C00)
+val Secondary80 = Color(0xFFFFA61A)
+val Secondary60 = Color(0xFFFFBA4D)
+val Secondary40 = Color(0xFFFFCE80)
+val Secondary20 = Color(0xFFFFE1B3)
+
+// Rating color
+val Rating = Color(0xFFFFAD30)
+
+// Warning colors
+val Warning100 = Color(0xFF804E00)
+val Warning20 = Color(0xFF995E00)
+
+// Success color
+val Success = Color(0xFF31B057)
