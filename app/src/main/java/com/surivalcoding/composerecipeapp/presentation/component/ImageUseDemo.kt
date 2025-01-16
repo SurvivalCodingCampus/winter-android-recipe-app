db06638ad86eaabeb6860e19f28ecb33a25723b1
SibangGyeon <briankim0695@gmail.com>
2025-01-16 11:14:55 +0900

    day21 : 함수명 파일명 통일 및 수정

---
+++ app/src/main/java/com/surivalcoding/composerecipeapp/presentation/component/ImageUseDemo.kt
@@ -0,0 +1,13 @@
+package com.surivalcoding.composerecipeapp.presentation.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.surivalcoding.composerecipeapp.model.Ingredient
+
+@Composable
+fun IngredientItem(
+    modifier: Modifier,
+    ingredient: Ingredient,
+) {
+
+}
