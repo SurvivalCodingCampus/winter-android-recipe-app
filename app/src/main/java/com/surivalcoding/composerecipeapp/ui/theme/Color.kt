b9a5f212c3ca30b1eb6ac4b82b7ff2a63c347e93
최현정 <hjchoi.dev@gmail.com>
2025-01-16 13:44:37 +0900

    chore: Add comment

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Color.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/theme/Color.kt
@@ -20,6 +20,7 @@ val Gray2 = Color(0xFF797979)
 val Gray3 = Color(0xFFA9A9A9)
 val Gray4 = Color(0xFFD9D9D9)
 
+// Primary colors
 val Primary100 = Color(0xFF129575)
 val Primary80 = Color(0xFF71B1A1)
 val Primary60 = Color(0xFFAFD3CA)
