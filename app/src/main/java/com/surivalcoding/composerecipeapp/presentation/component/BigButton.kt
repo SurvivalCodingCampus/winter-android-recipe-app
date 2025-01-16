c21914176689415c98fa6ba4a1cb11a46b027731
SibangGyeon <briankim0695@gmail.com>
2025-01-16 11:08:24 +0900

    chore : 네트워크 연결 추가 및 의존성 추가

--- app/src/main/java/com/surivalcoding/composerecipeapp/presentation/component/BigButton.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/presentation/component/BigButton.kt
@@ -2,9 +2,12 @@ package com.surivalcoding.composerecipeapp.presentation.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -14,9 +17,14 @@ import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.surivalcoding.composerecipeapp.ui.AppColors
@@ -28,16 +36,24 @@ fun BigButton(
     text: String,
     onClick: () -> Unit = {},
 ) {
+    var isPressed by remember { mutableStateOf(false) }
     Box(
         modifier = modifier
             .width(315.dp)
             .height(60.dp)
             .background(
-                color = AppColors.primary,
+                color = if (isPressed) Color.Gray else AppColors.primary100,
                 shape = RoundedCornerShape(10.dp),
             )
-            .clickable {
-                onClick()
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onPress = {
+                        isPressed = true
+                        tryAwaitRelease()
+                        isPressed = false
+                        onClick()
+                    }
+                )
             },
         contentAlignment = Alignment.Center,
     ) {
@@ -70,7 +86,12 @@ fun BigButton(
 @Preview(showBackground = true)
 @Composable
 private fun BigButtonPreview1() {
-    BigButton(
-        text = "Button"
-    )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        BigButton(
+            text = "Button"
+        )
+    }
 }
