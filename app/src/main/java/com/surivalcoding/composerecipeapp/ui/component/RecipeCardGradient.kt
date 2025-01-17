a79ad7f94461f1903bc0b1e2359c6163dc2cc274
최현정 <hjchoi.dev@gmail.com>
2025-01-17 19:08:30 +0900

    feat: Add RecipeCardGradient

---
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/component/RecipeCardGradient.kt
@@ -0,0 +1,11 @@
+package com.surivalcoding.composerecipeapp.ui.component
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val RecipeCardGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color.Transparent,
+        Color.Black
+    )
+)
