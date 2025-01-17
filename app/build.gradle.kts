7f1cf5e4edbf8b017f8c4e4e245b973f927d379a
choe-yujin <dev.yujinchoe@gmail.com>
2025-01-18 05:31:52 +0900

    build: Add dependencies for JSON parsing and serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.constraintlayout.compose)
+    implementation(libs.firebase.perf.ktx)
+    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
