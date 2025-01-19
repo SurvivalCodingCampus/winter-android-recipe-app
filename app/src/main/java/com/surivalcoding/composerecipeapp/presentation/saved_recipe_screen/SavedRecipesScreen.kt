c40186f4e33a6b5c80e5a27852eaf220542bb8be
SibangGyeon <briankim0695@gmail.com>
2025-01-20 03:12:37 +0900

    feat : SavedRecipeScreen view, view model 추가

---
+++ app/src/main/java/com/surivalcoding/composerecipeapp/presentation/saved_recipe_screen/SavedRecipesScreen.kt
@@ -0,0 +1,86 @@
+package com.surivalcoding.composerecipeapp.presentation.saved_recipe_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.surivalcoding.composerecipeapp.data.model.Recipe
+import com.surivalcoding.composerecipeapp.presentation.component.RecipeCard
+import com.surivalcoding.composerecipeapp.ui.AppColors
+import com.surivalcoding.composerecipeapp.ui.AppTextStyles
+
+@Composable
+fun SavedRecipesScreen(
+    modifier: Modifier = Modifier,
+    recipes: List<Recipe>,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Column(
+            modifier = Modifier
+                .padding(top = 54.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            Text(
+                text = "Saved Recipes",
+                style = AppTextStyles.mediumTextBold.copy(
+                    color = AppColors.font
+                )
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(629.dp)
+                    .background(color = AppColors.white)
+                    .padding(top = 10.dp, start = 30.dp, end = 30.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                items(recipes) { recipe ->
+                    RecipeCard(
+                        recipe = recipe,
+                        modifier = Modifier
+                    )
+                }
+            }
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SavedRecipesScreenPreview() {
+    val recipe1 = Recipe(
+        "Italian",
+        1,
+        "Traditional spare ribs baked",
+        "https://www.foodnews.news/data/photos/20210728/art_16261398155074_8642d5.jpg",
+        "Chef John",
+        "20 min",
+        4.0,
+        emptyList(),
+    )
+    val recipes = listOf(recipe1, recipe1, recipe1, recipe1, recipe1)
+
+    SavedRecipesScreen(
+        recipes = recipes
+    )
+}
