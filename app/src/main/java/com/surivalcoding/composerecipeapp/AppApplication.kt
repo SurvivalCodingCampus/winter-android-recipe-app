6866fd374501c7df4d4167f85250485be027c811
steve <diasm2@gmail.com>
2025-01-21 08:32:18 +0900

    feat: Initialize repositories in AppApplication
    This commit introduces the following changes to the `AppApplication` class:
    
    -   Added `userDataSource` initialization.
    -   Added `userRepository` initialization.
    -  Added a lazy initialization for `userDataSource` and `userRepository`.
    - The `recipeRepository` remains initialized as it was before.
    - The `mockRecipeDataSource` and the `recipeRepository` continue to be initialized as they were before.
    - Removed the commented line of `recipeDataSource`.
    - Added `UserRepositoryImpl` and `UserDataSourceImpl`.
    - Added `RecipeRepositoryImpl` and `MockRecipeDataSourceImpl`.

--- app/src/main/java/com/surivalcoding/composerecipeapp/AppApplication.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/AppApplication.kt
@@ -1,10 +1,16 @@
 package com.surivalcoding.composerecipeapp
 import android.app.Application
-import com.surivalcoding.composerecipeapp.data.repository.RecipeRepositoryImpl
-import com.surivalcoding.composerecipeapp.data.data_source.MockRecipeDataSourceImpl
+import com.surivalcoding.composerecipeapp.data.chef.UserRepositoryImpl
+import com.surivalcoding.composerecipeapp.data.chef.data_source.UserDataSourceImpl
+import com.surivalcoding.composerecipeapp.data.recipe.RecipeRepositoryImpl
+import com.surivalcoding.composerecipeapp.data.recipe.data_source.MockRecipeDataSourceImpl
+import com.surivalcoding.composerecipeapp.data.recipe.data_source.RecipeDataSourceImpl
 
 
 class AppApplication: Application(){
     private val mockRecipeDataSource by lazy { MockRecipeDataSourceImpl() }
+//    private val recipeDataSource by lazy { RecipeDataSourceImpl() }
+    private val userDataSource by lazy {UserDataSourceImpl()}
     val recipeRepository by lazy { RecipeRepositoryImpl(mockRecipeDataSource) }
+    val userRepository by lazy { UserRepositoryImpl(userDataSource) }
 }
