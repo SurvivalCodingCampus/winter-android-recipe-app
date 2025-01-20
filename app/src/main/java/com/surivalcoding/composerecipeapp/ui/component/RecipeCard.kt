feff514aade586f8193f5111627598bf71b34593
최현정 <hjchoi.dev@gmail.com>
2025-01-20 13:40:30 +0900

    design: Update Rating layout

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/component/RecipeCard.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/component/RecipeCard.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -92,12 +93,14 @@ fun RecipeCard(
                 Icon(
                     modifier = Modifier
                         .width(12.dp)
-                        .align(Alignment.CenterVertically),
+                        .alignBy(LastBaseline),
                     imageVector = Icons.Filled.Star,
                     contentDescription = "Rating",
                     tint = Rating
                 )
                 Text(
+                    modifier = Modifier
+                        .align(Alignment.Bottom),
                     text = rating,
                     style = PoppinsRegularTypography.bodySmall.copy(
                         fontSize = 8.sp,
