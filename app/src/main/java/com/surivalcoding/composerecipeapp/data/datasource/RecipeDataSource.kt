0da85c1a3ec6c3c4dca694bdc9c7703ac2113b1e
hodak <jlkim909@gmail.com>
2025-01-21 00:48:15 +0900

    feat: getSearchRecipes 추가

--- app/src/main/java/com/surivalcoding/composerecipeapp/data/datasource/RecipeDataSource.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/data/datasource/RecipeDataSource.kt
@@ -1,8 +1,10 @@
 package com.surivalcoding.composerecipeapp.data.datasource
 
 import com.surivalcoding.composerecipeapp.data.model.SavedRecipe
+import com.surivalcoding.composerecipeapp.data.model.SearchRecipe
 import kotlinx.coroutines.flow.Flow
 
 interface RecipeDataSource {
+    fun getSearchRecipes(query: String): Flow<List<SearchRecipe>>
     fun getSavedRecipe(ids: List<Int>): Flow<List<SavedRecipe>>
 }
--- app/src/main/java/com/surivalcoding/composerecipeapp/data/datasource/RecipeDataSourceImpl.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/data/datasource/RecipeDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.surivalcoding.composerecipeapp.data.datasource
 
+import com.surivalcoding.composerecipeapp.data.mock.fakeSearchRecipe
 import com.surivalcoding.composerecipeapp.data.model.SavedRecipe
+import com.surivalcoding.composerecipeapp.data.model.SearchRecipe
 import com.surivalcoding.composerecipeapp.util.BASE_URL
 import com.surivalcoding.composerecipeapp.util.createJsonHttpClient
 import io.ktor.client.HttpClient
@@ -13,6 +15,12 @@ import kotlinx.serialization.ExperimentalSerializationApi
 class RecipeDataSourceImpl @OptIn(ExperimentalSerializationApi::class) constructor(
     private val httpClient: HttpClient = createJsonHttpClient()
 ) : RecipeDataSource {
+    override fun getSearchRecipes(query: String): Flow<List<SearchRecipe>> = flow {
+        emit(fakeSearchRecipe.filter {
+            query in it.title || query in it.authorName
+        })
+    }
+
     override fun getSavedRecipe(ids: List<Int>): Flow<List<SavedRecipe>> = flow {
         httpClient.use { client ->
             emit(client.get(BASE_URL).body<List<SavedRecipe>>())
