631d4384cd414f24bae8962f112c6a3a913ec3c4
hodak <jlkim909@gmail.com>
2025-01-21 00:49:08 +0900

    feat: SearchRecipeViewModel 구현

--- app/src/main/java/com/surivalcoding/composerecipeapp/AppApplication.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/AppApplication.kt
@@ -1,13 +1,33 @@
 package com.surivalcoding.composerecipeapp
 
 import android.app.Application
+import com.surivalcoding.composerecipeapp.data.datasource.RecentSearchRecipeRepositoryImpl
 import com.surivalcoding.composerecipeapp.data.datasource.RecipeDataSourceImpl
 import com.surivalcoding.composerecipeapp.data.datasource.RecipeRepositoryImpl
+import com.surivalcoding.composerecipeapp.data.mock.savedRecipeMockEngine
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 
 class AppApplication : Application() {
+    @OptIn(ExperimentalSerializationApi::class)
     val recipeRepository by lazy {
         RecipeRepositoryImpl(
-            RecipeDataSourceImpl()
+            RecipeDataSourceImpl(
+                httpClient = HttpClient(savedRecipeMockEngine) {
+                    install(ContentNegotiation) {
+                        json(Json {
+                            ignoreUnknownKeys = true
+                            allowTrailingComma = true
+                        })
+                    }
+                }
+            )
         )
+    }
+    val recentSearchRecipeRepository by lazy {
+        RecentSearchRecipeRepositoryImpl()
     }
 }
