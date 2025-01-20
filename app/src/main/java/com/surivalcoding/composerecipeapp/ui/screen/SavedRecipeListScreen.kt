072738afe34071b75dc829acd50997533b5be115
최현정 <hjchoi.dev@gmail.com>
2025-01-20 18:55:46 +0900

    feat: Update backgroundColor

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/screen/SavedRecipeListScreen.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/screen/SavedRecipeListScreen.kt
@@ -1,5 +1,6 @@
 package com.surivalcoding.composerecipeapp.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.surivalcoding.composerecipeapp.data.local.mock.MockRecipeData
@@ -36,7 +38,8 @@ fun SavedRecipeListScreen() {
 
     Column(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
