d4c928f673ad79df87c125fcd0ed135999472f24
hodak <jlkim909@gmail.com>
2025-01-21 19:00:14 +0900

    refactor: book mark icon 변경

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/CraIcons.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/CraIcons.kt
@@ -4,8 +4,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.automirrored.rounded.ArrowForward
 import androidx.compose.material.icons.rounded.Alarm
-import androidx.compose.material.icons.rounded.Bookmark
-import androidx.compose.material.icons.rounded.BookmarkBorder
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material.icons.rounded.StarBorder
 import androidx.compose.material.icons.rounded.StarRate
@@ -17,8 +15,7 @@ object CraIcons {
     val star = Icons.Rounded.Star
     val starRate = Icons.Rounded.StarRate
     val starBorder = Icons.Rounded.StarBorder
-    val bookMarkBorder = Icons.Rounded.BookmarkBorder
-    val bookMark = Icons.Rounded.Bookmark
+    val bookMark = R.drawable.ic_outlined_bookmark
     val alarm = Icons.Rounded.Alarm
     val outlinedSetting = R.drawable.ic_outline_setting
     val searchNormal = R.drawable.ic_search_normal
