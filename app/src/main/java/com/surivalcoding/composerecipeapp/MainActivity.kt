30c13cbed47256b0139da2f956dc3422b7300bc1
steve <diasm2@gmail.com>
2025-01-21 19:59:57 +0900

    feat: Implement NavigationRoot and remove unused code
    This commit introduces the `NavigationRoot` composable, which is now used as the main entry point for navigation within the app. Additionally, it removes unused code related to old routing logic. The `MainActivity` now uses `NavigationRoot` to manage navigation flow.

--- app/src/main/java/com/surivalcoding/composerecipeapp/MainActivity.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/MainActivity.kt
@@ -1,28 +1,13 @@
 package com.surivalcoding.composerecipeapp
 
-import RecipeDtoRouter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.surivalcoding.composerecipeapp.presentation.saved_recipes.RecipeViewModel
-import com.surivalcoding.composerecipeapp.ui.theme.ComposeRecipeAppTheme
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import com.surivalcoding.composerecipeapp.navigation.NavigationRoot
-import com.surivalcoding.composerecipeapp.presentation.saved_recipes.SavedRecipesScreen
-import com.surivalcoding.composerecipeapp.presentation.recipe_detail.RecipeDetailScreen
-import com.surivalcoding.composerecipeapp.presentation.saved_recipes.view_model.RecipeDetailViewModel
-import com.surivalcoding.composerecipeapp.presentation.saved_recipes.view_model.RecipeSearchViewModel
-import com.surivalcoding.composerecipeapp.presentation.recently_search_recipe.RecipeSearchScreen
-import kotlinx.serialization.Serializable
+import com.surivalcoding.composerecipeapp.ui.theme.ComposeRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,5 +26,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ComposeRecipeAppTheme {
+        MainActivity()
     }
 }
