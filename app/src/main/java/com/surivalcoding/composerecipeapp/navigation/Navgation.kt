18d7b98dcf66d971052130992f57fc4c8cea8c62
steve <diasm2@gmail.com>
2025-01-22 10:41:04 +0900

    feat: Implement bottom navigation bar and navigation logic
    This commit introduces the following changes:
    
    - Added a bottom navigation bar with Home and Recently Search Recipes buttons.
    - Implemented navigation logic to switch between screens using the bottom navigation bar.
    - Updated the navigation graph to include the new screens and routes.
    - Refactored the navigation setup for improved clarity and structure.

--- app/src/main/java/com/surivalcoding/composerecipeapp/navigation/Navgation.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/navigation/Navgation.kt
@@ -7,79 +7,90 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavGraphBuilder
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.surivalcoding.composerecipeapp.presentation.bottomAppBar.BottomAppBarScreen
+import com.surivalcoding.composerecipeapp.presentation.home.HomeScreen
 import com.surivalcoding.composerecipeapp.presentation.recently_search_recipe.RecipeSearchScreen
 import com.surivalcoding.composerecipeapp.presentation.recipe_detail.RecipeDetailScreen
 import com.surivalcoding.composerecipeapp.presentation.saved_recipes.RecipeViewModel
 import com.surivalcoding.composerecipeapp.presentation.saved_recipes.SavedRecipesScreen
+import com.surivalcoding.composerecipeapp.presentation.home.HomeViewModel
 import com.surivalcoding.composerecipeapp.presentation.saved_recipes.view_model.RecipeDetailViewModel
 import com.surivalcoding.composerecipeapp.presentation.saved_recipes.view_model.RecipeSearchViewModel
 import com.surivalcoding.composerecipeapp.presentation.sign_in.SignInScreen
 import com.surivalcoding.composerecipeapp.presentation.sign_up.SignUpScreen
 import com.surivalcoding.composerecipeapp.presentation.splash.SplashScreen
-import com.surivalcoding.composerecipeapp.presentation.splash.SplashState
 import com.surivalcoding.composerecipeapp.presentation.splash.SplashViewModel
-import kotlinx.serialization.Serializable
-
-
-@Serializable
-object Main
-
-@Serializable
-object RecipeRouter
-
-sealed interface Route {
-
-    @Serializable
-    object Main : Route
-
-    @Serializable
-    object Recipe : Route
-
-    @Serializable
-    object SignIn : Route
-
-    @Serializable
-    object SignUp : Route
-
-    @Serializable
-    data class RecipeDetail(val userId: Int, val chefName: String) : Route
-
-    @Serializable
-    data class User(val userId: Int, val chefName: String) : Route
-
-    @Serializable
-    object RecentlySearchRecipe : Route
-}
 
 @Composable
-fun NavigationRoot(
-) {
+fun NavigationRoot() {
     val navController = rememberNavController()
 
+
     NavHost(
-        navController = navController, startDestination = Route.Main
+        navController = navController, startDestination = Route.Splash,
     ) {
-        composable<Route.Main> {
+        composable<Route.Splash> {
+            val homeViewModel = viewModel<HomeViewModel>(factory = HomeViewModel.Factory)
             val viewModel = viewModel<SplashViewModel>(factory = SplashViewModel.Factory)
             val state = viewModel.state.collectAsStateWithLifecycle().value
 
-            SplashScreen(state, onStartClick = {
-                toSignInGraph(navController)
-            })
-        }
+            homeViewModel.selectRoute(Route.Splash)
 
+            SplashScreen(
+                state, onStartClick = {
+                    toSignInGraph(navController)
+                }
+            )
+        }
         composable<Route.SignIn> {
-            Scaffold { innerPadding ->
-                SignInScreen(modifier = Modifier.padding(innerPadding),
+            NavigationMain()
+        }
+    }
+}
+
+@Composable
+fun NavigationMain() {
+    val navController = rememberNavController()
+    val viewModel = viewModel<HomeViewModel>(factory = HomeViewModel.Factory)
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBarScreen(state = state,
+                onClickToHome = {
+                    viewModel.selectRoute(Route.Home)
+                    navController.navigate(Route.Recipe)
+                },
+                onClickToSavedRecipes = {
+                    viewModel.selectRoute(Route.RecentlySearchRecipe)
+                    navController.navigate(Route.RecentlySearchRecipe)
+                })
+
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController, startDestination = Route.SignIn,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable<Route.Home> {
+                HomeScreen(
+                    state = state,
+                    modifier = Modifier,
+                    onValueChange = {},
+                    onSearching = {}
+                )
+            }
+            composable<Route.SignIn> {
+                SignInScreen(modifier = Modifier,
                     onSignInClick = {
-                        toRecipeGraph(navController)
+                        toHomeGraph(navController)
                     },
                     onForgotPasswordClick = {},
                     onGoogleSignInClick = {},
@@ -87,30 +98,24 @@ fun NavigationRoot(
                     onSignUpClick = {
                         toSignUpGraph(navController)
                     })
-
             }
-
-        }
-
-        composable<Route.SignUp> {
-            Scaffold { innerPadding ->
-                SignUpScreen(modifier = Modifier.padding(innerPadding), onSignUpClick = {
+            composable<Route.SignUp> {
+                SignUpScreen(modifier = Modifier, onSignUpClick = {
                     toSignUpGraph(navController)
                 }, onGoogleSignInClick = {}, onFacebookSignInClick = {}, onSignInClick = {
                     toSignInGraph(navController)
                 }) { }
 
+
             }
+            composable<Route.Recipe> { backStackEntry ->
+                val recipeViewModel = viewModel<RecipeViewModel>(factory = RecipeViewModel.Factory)
+                val recipeState by recipeViewModel.state.collectAsStateWithLifecycle()
 
-        }
-        composable<Route.Recipe> { backStackEntry ->
-            val viewModel = viewModel<RecipeViewModel>(factory = RecipeViewModel.Factory)
-            val state by viewModel.state.collectAsStateWithLifecycle()
 
-            Scaffold { innerPadding ->
                 SavedRecipesScreen(
-                    modifier = Modifier.padding(innerPadding),
-                    state = state,
+                    modifier = Modifier,
+                    state = recipeState,
                     onRecipeClick = { recipe ->
                         toRecipeDetailGraph(navController, userId = 1, recipe = recipe)
                     },
@@ -118,35 +123,32 @@ fun NavigationRoot(
                 )
 
             }
-        }
-        composable<Route.RecipeDetail> { backStackEntry ->
-            backStackEntry.toRoute<Route.Recipe>()
-            val viewModel =
-                viewModel<RecipeDetailViewModel>(factory = RecipeDetailViewModel.Factory)
-            val state by viewModel.state.collectAsStateWithLifecycle()
-            Scaffold { innerPadding ->
+            composable<Route.RecipeDetail> { backStackEntry ->
+                backStackEntry.toRoute<Route.Recipe>()
+                val recipeDetailViewModel =
+                    viewModel<RecipeDetailViewModel>(factory = RecipeDetailViewModel.Factory)
+                val recipeDetailState by recipeDetailViewModel.state.collectAsStateWithLifecycle()
                 RecipeDetailScreen(
-                    state = state,
-                    modifier = Modifier.padding(innerPadding),
+                    state = recipeDetailState,
+                    modifier = Modifier,
                     onBackClick = { navController.popBackStack() },
                 )
 
             }
+
+            composable<Route.RecentlySearchRecipe> { backStackEntry ->
+                val recipeSearchViewModel =
+                    viewModel<RecipeSearchViewModel>(factory = RecipeSearchViewModel.Factory)
+                val recipeSearchState by recipeSearchViewModel.state.collectAsStateWithLifecycle()
+
+                RecipeSearchScreen(state = recipeSearchState,
+                    onValueChange = { newKeyword -> recipeSearchViewModel.searchRecipes(newKeyword) },
+                    onSearching = { isSearching -> recipeSearchViewModel.onSearching(isSearching) })
+            }
         }
-
-
-        composable<Route.RecentlySearchRecipe> { backStackEntry ->
-            val viewModel =
-                viewModel<RecipeSearchViewModel>(factory = RecipeSearchViewModel.Factory)
-            val state by viewModel.state.collectAsStateWithLifecycle()
-
-            RecipeSearchScreen(state = state,
-                onValueChange = { newKeyword -> viewModel.searchRecipes(newKeyword) },
-                onSearching = { isSearching -> viewModel.onSearching(isSearching) })
-        }
-
     }
 }
+
 
 private fun NavGraphBuilder.toMainGraph(navController: NavHostController) {
     navController.navigate(Route.SignIn) {
@@ -156,9 +158,14 @@ private fun NavGraphBuilder.toMainGraph(navController: NavHostController) {
     }
 }
 
+fun NavGraphBuilder.toHomeGraph(navController: NavHostController) {
+    navController.navigate(Route.Home) {
+    }
+}
+
 private fun NavGraphBuilder.toSignInGraph(navController: NavHostController) {
     navController.navigate(Route.SignIn) {
-        popUpTo(Route.SignUp) {
+        popUpTo(Route.Splash) {
             inclusive = true
         }
     }
@@ -172,7 +179,7 @@ private fun NavGraphBuilder.toSignUpGraph(navController: NavHostController) {
     }
 }
 
-private fun NavGraphBuilder.toRecipeGraph(navController: NavHostController) {
+fun NavGraphBuilder.toRecipeGraph(navController: NavHostController) {
     navController.navigate(Route.Recipe)
 }
 
