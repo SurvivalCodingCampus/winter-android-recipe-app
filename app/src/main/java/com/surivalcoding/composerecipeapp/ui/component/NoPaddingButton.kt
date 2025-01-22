c5b8aa2a8e7ffc94d0aef7fd2087ecb761babc44
hodak <jlkim909@gmail.com>
2025-01-23 00:00:44 +0900

    refactor: NoPaddingSurface 삭제
    LocalMinimumInteractiveComponentSize 으로 기존 Surface에 패딩 제거하여 사용

--- app/src/main/java/com/surivalcoding/composerecipeapp/ui/component/NoPaddingButton.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/ui/component/NoPaddingButton.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
@@ -21,6 +23,7 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -40,29 +43,31 @@ fun NoPaddingButton(
     val containerColor = if (enabled) colors.containerColor else colors.disabledContainerColor
     val contentColor = if (enabled) colors.contentColor else colors.disabledContentColor
 
-    NoPaddingSurface(
-        onClick = onClick,
-        modifier = modifier
-            .semantics { role = Role.Button },
-        enabled = enabled,
-        shape = shape,
-        color = containerColor,
-        contentColor = contentColor,
-        border = border,
-        interactionSource = interactionSource
-    ) {
-        val mergedStyle = LocalTextStyle.current.merge(MaterialTheme.typography.labelLarge)
-        CompositionLocalProvider(
-            LocalContentColor provides contentColor,
-            LocalTextStyle provides mergedStyle,
+    CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides Dp.Unspecified) {
+        Surface(
+            onClick = onClick,
+            modifier = modifier
+                .semantics { role = Role.Button },
+            enabled = enabled,
+            shape = shape,
+            color = containerColor,
+            contentColor = contentColor,
+            border = border,
+            interactionSource = interactionSource
         ) {
-            Row(
-                Modifier
-                    .padding(contentPadding),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-                content = content
-            )
+            val mergedStyle = LocalTextStyle.current.merge(MaterialTheme.typography.labelLarge)
+            CompositionLocalProvider(
+                LocalContentColor provides contentColor,
+                LocalTextStyle provides mergedStyle,
+            ) {
+                Row(
+                    Modifier
+                        .padding(contentPadding),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = content
+                )
+            }
         }
     }
 }
