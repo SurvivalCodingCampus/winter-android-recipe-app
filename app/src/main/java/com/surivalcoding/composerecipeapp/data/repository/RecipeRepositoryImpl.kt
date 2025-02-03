86e91a899ef6203ff7b17eaf3e98531c35da4336
hodak <jlkim909@gmail.com>
2025-02-03 20:39:57 +0900

    refactor: getSavedRecipes 삭제

--- app/src/main/java/com/surivalcoding/composerecipeapp/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/data/repository/RecipeRepositoryImpl.kt
@@ -3,8 +3,6 @@ package com.surivalcoding.composerecipeapp.data.repository
 import com.surivalcoding.composerecipeapp.data.datasource.RecipeDataSource
 import com.surivalcoding.composerecipeapp.data.model.Recipe
 import com.surivalcoding.composerecipeapp.data.model.RecipeCategory
-import com.surivalcoding.composerecipeapp.data.model.SavedRecipe
-import com.surivalcoding.composerecipeapp.data.model.SearchRecipe
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -14,12 +12,6 @@ class RecipeRepositoryImpl @Inject constructor(
 ) : RecipeRepository {
     override fun getRecipes(): Flow<List<Recipe>> =
         recipeDataSource.getRecipes()
-
-    override fun getSearchRecipes(query: String): Flow<List<SearchRecipe>> =
-        recipeDataSource.getSearchRecipes(query)
-
-    override fun getSavedRecipes(ids: List<Int>): Flow<List<SavedRecipe>> =
-        recipeDataSource.getSavedRecipe(ids)
 
     override fun getRecipesByCategory(category: RecipeCategory): Flow<List<Recipe>> {
         val recipes = getRecipes()
