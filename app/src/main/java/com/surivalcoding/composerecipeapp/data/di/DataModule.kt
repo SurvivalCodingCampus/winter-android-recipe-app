ced0854037e580e74256e8dd026c70736853ce5a
hodak <jlkim909@gmail.com>
2025-02-03 20:34:23 +0900

    refactor: RecentSearchRecipeRepository.kt 삭제

--- app/src/main/java/com/surivalcoding/composerecipeapp/data/di/DataModule.kt
+++ app/src/main/java/com/surivalcoding/composerecipeapp/data/di/DataModule.kt
@@ -6,8 +6,6 @@ import com.surivalcoding.composerecipeapp.data.datasource.FakePreferenceDataSour
 import com.surivalcoding.composerecipeapp.data.datasource.PreferenceDataSource
 import com.surivalcoding.composerecipeapp.data.datasource.RecipeDataSource
 import com.surivalcoding.composerecipeapp.data.datasource.RecipeDataSourceImpl
-import com.surivalcoding.composerecipeapp.data.repository.RecentSearchRecipeRepository
-import com.surivalcoding.composerecipeapp.data.repository.RecentSearchRecipeRepositoryImpl
 import com.surivalcoding.composerecipeapp.data.repository.RecipeRepository
 import com.surivalcoding.composerecipeapp.data.repository.RecipeRepositoryImpl
 import com.surivalcoding.composerecipeapp.data.repository.UserDataRepository
@@ -42,10 +40,6 @@ abstract class DataModule {
         preferenceDataSource: FakePreferenceDataSource
     ): PreferenceDataSource
 
-    @Binds
-    abstract fun bindRecentSearchRecipeRepository(
-        recentSearchRecipeRepository: RecentSearchRecipeRepositoryImpl
-    ): RecentSearchRecipeRepository
 
     @Binds
     abstract fun bindsNetworkMonitor(
